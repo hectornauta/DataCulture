1e8b1da338d4b7ab2e4db0715a02223739c10610
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2022-01-05 18:08:54 -0300

    Cambio menor en la conexión a la BD

--- ejecutar_scripts_sql.py
+++ ejecutar_scripts_sql.py
@@ -1,15 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
-import sqlalchemy as db
+import db
 import settings
 import sqlalchemy_utils
 from sqlalchemy_utils import database_exists, create_database
 
-usuario = settings.DATABASES['default']['USER']
-contrasena = settings.DATABASES['default']['PASSWORD']
-host = settings.DATABASES['default']['HOST']
-db_nombre = settings.DATABASES['default']['NAME']
-engine = db.create_engine('postgresql+psycopg2://'+ usuario +':' + contrasena + '@' + host +'/'+ db_nombre)
+engine = db.conectar()
 
 if not database_exists(engine.url):
     create_database(engine.url)
